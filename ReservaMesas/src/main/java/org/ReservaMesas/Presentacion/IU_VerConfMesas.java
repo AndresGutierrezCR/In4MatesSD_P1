a5f238bb2612405176eefc4a46f079bf29462afe
DavidCarneros <David.Carneros1@alu.uclm.es>
2018-10-30 19:09:31 +0100

    Añadida la interfaz configuracion mesas

--- ReservaMesas/src/main/java/org/ReservaMesas/Presentacion/IU_VerConfMesas.java
+++ ReservaMesas/src/main/java/org/ReservaMesas/Presentacion/IU_VerConfMesas.java
@@ -1,13 +1,31 @@
 package org.ReservaMesas.Presentacion;
 
 import javax.swing.JPanel;
+import java.awt.Color;
+import javax.swing.border.TitledBorder;
+import java.awt.BorderLayout;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 public class IU_VerConfMesas extends JPanel {
+	private JScrollPane scrollPane;
+	private JTable tableMesas;
 
 	/**
 	 * Create the panel.
 	 */
 	public IU_VerConfMesas() {
+		setBorder(new TitledBorder(null, "Configuraci\u00F3n mesas", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		setForeground(Color.LIGHT_GRAY);
+		setLayout(new BorderLayout(0, 0));
+		{
+			scrollPane = new JScrollPane();
+			add(scrollPane, BorderLayout.CENTER);
+			{
+				tableMesas = new JTable();
+				scrollPane.setViewportView(tableMesas);
+			}
+		}
 
 	}
 
