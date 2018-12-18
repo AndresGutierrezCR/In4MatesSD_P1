e5a5885c14f2881a2ca83cad87065425cb2c0da3
Andrés Gutiérrez Cepeda <Vaogaz@DESKTOP-IA3MRJK>
2018-12-18 14:00:45 +0100

    Corrección de errores de checklist del paquete autenticación.

--- Autenticacion/src/main/java/org/Autenticacion/Persistencia/Agente.java
+++ Autenticacion/src/main/java/org/Autenticacion/Persistencia/Agente.java
@@ -3,17 +3,17 @@
  **/
 package org.Autenticacion.Persistencia;
 
-import java.awt.List;
-import java.io.File;
+
 import java.sql.*;
-import java.util.LinkedList;
-import java.util.Vector;
 
 /**
- * Clase Agente. Utilizada para realizar la conexión y las distintas interacciones con la base de datos.
- * Solo podrá crearse un agente a la vez y controlaremos esto mediante la instancia del agente
- * se realizara la conexión a la base de datos que estará contenida en un fichero access
- * y se hará uso de los drivers necesarios para su correcto funcionamiento.
+ * Clase Agente. Utilizada para realizar la conexión y las
+ * distintas interacciones con la base de datos.
+ * Solo podrá crearse un agente a la vez y controlaremos
+ * esto mediante la instancia del agente se realizara
+ * la conexión a la base de datos que estará contenida en un
+ * fichero access y se hará uso de los drivers necesarios
+ * para su correcto funcionamiento.
  *
  * @author in4mates
  * @version 1.0
@@ -29,7 +29,8 @@ public class Agente {
 	 **/
     protected static Connection mBD;
     /**
-	 * Dirección en la que se encuentra la base de datos sobre la que se trabaja.
+	 * Dirección en la que se encuentra la base de datos sobre la que
+	 * se trabaja.
 	 **/
     private static String url = "jdbc:ucanaccess://in4mates.accdb";
     /**
@@ -39,7 +40,7 @@ public class Agente {
 
 	/**
 	 * Constructor por defecto. Realiza la conexión a la base de datos.
-	 * @throws Exception.
+	 * @throws Exception .
 	 */
     private Agente() throws Exception {
         conectar();
@@ -47,26 +48,26 @@ public class Agente {
     }
 
 	/**
-	 * Constructor que creará y devolverá un agente en caso de que no exista uno previamente, si existe un
+	 * Constructor que creará y devolverá un agente en caso
+	 * de que no exista uno previamente, si existe un
 	 * agente con anterioridad se devolverá dicho agente.
 	 * @return Instancia del agente.
 	 * @throws Exception .
 	 */
     public static Agente getAgente() throws Exception {
-    //	File miDir = new File (".");
-    //	System.out.println(miDir.getCanonicalPath().toString());
         if (mInstancia == null) {
             mInstancia = new Agente();
         }
         return mInstancia;
     }
 	/**
-	 * Realiza la conexión con la base de datos mediante el uso del driver y de la url.
-	 * @throws Exception.
+	 * Realiza la conexión con la base de datos mediante
+	 * el uso del driver y de la url.
+	 * @throws Exception .
 	 */
     private void conectar() throws Exception {
         Class.forName(driver);
-        mBD = DriverManager.getConnection(url, "", "");;
+        mBD = DriverManager.getConnection(url, "", "");
     }
 
 	/**
@@ -78,38 +79,34 @@ public class Agente {
     }
 
 	/**
-	 * Comunica a la base de datos la consulta SQL de modificación que ha recibido el 
-	 * agente por parte del gestor.
-	 * @throws Exception . 
+	 * Comunica a la base de datos la consulta SQL de modificación
+	 * que ha recibido el agente por parte del gestor.
+	 * @throws Exception .
 	 * @throws SQLException .
 	 * @param SQL (Consulta SQL de modificación sobre la base de datos).
-	 * @return Resultado de la consulta SQL (si esta se ha realizado de manera correcta).
+	 * @return Resultado de la consulta SQL (si esta se ha realizado
+	 * de manera correcta).
 	 */
     public int modificar(String SQL) throws SQLException, Exception {
-      //  PreparedStatement stmt = mBD.prepareStatement(SQL);
-        Statement st =mBD.createStatement();
-     //   int res = stmt.executeUpdate();
+        Statement st = mBD.createStatement();
         int res = st.executeUpdate(SQL);
         st.close();
-     //   desconectar();
         return res;
     }
 
     /**
-	 * Comunica a la base de datos la consulta SQL de lectura de datos que ha sido entregada al agente por
-	 * el gestor.
+	 * Comunica a la base de datos la consulta SQL de lectura de datos
+	 * que ha sido entregada al agente por el gestor.
 	 * @throws Exception .
 	 * @throws SQLException .
 	 * @param SQL (Consulta SQL de lectura de datos sobre la base de datos).
-	 * @return Resultado de la consulta SQL (Datos obtenidos de la base de datos).
+	 * @return Resultado de la consulta SQL (Datos obtenidos de la
+	 * base de datos).
 	 */
     public ResultSet leer(String SQL) throws SQLException, Exception {
-        
-    	Statement st =mBD.createStatement();
-        //  PreparedStatement stmt = mBD.prepareStatement(SQL);
-         // ResultSet resultado = stmt.executeQuery(SQL);
-      	ResultSet resultado = st.executeQuery(SQL);
-      	
+    	Statement st = mBD.createStatement();
+      	ResultSet resultado = st.executeQuery(SQL);	
         return resultado;
     }
+    
 }
--- Autenticacion/src/main/java/org/Autenticacion/Persistencia/GestorUsuario.java
+++ Autenticacion/src/main/java/org/Autenticacion/Persistencia/GestorUsuario.java
@@ -2,6 +2,7 @@
  * Paquete que contiene las clases de persistencia del módulo autenticarse.
  **/
 package org.Autenticacion.Persistencia;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -9,9 +10,10 @@ import java.util.ArrayList;
 import org.Autenticacion.Dominio.Usuario;
 
 /**
- * Clase GestorUsuario. Utilizada como intermediario entre el usuario de la aplicación
- * y el agente que se conecta a la base de datos para realizar las distintas operaciones.
- * Es la encargada de realizar todas las consultas SQL de lectura, eliminación, modificación y adición.
+ * Clase GestorUsuario. Utilizada como intermediario entre el usuario de la
+ * aplicación y el agente que se conecta a la base de datos para realizar las
+ * distintas operaciones. Es la encargada de realizar todas las consultas SQL de
+ * lectura, eliminación, modificación y adición.
  *
  * @author in4mates
  * @version 1.0
@@ -20,146 +22,175 @@ import org.Autenticacion.Dominio.Usuario;
 public class GestorUsuario {
 
 	/**
-	 * Array list que contendrá a los distintos usuarios que se lean de la base de datos.
+	 * Array list que contendrá a los distintos usuarios que se lean de
+	 * la base de datos.
 	 **/
-	private ArrayList <Usuario> listaUsuarios;
-	
+	private ArrayList<Usuario> listaUsuarios;
+
 	/**
-	 * Constructor por defecto. Realiza la creacion de la lista vacía de usuarios.
+	 * Constructor por defecto. Realiza la creacion de la lista vacía de
+	 * usuarios.
 	 */
 	public GestorUsuario() {
-		this.listaUsuarios=new ArrayList<Usuario>();
+		this.listaUsuarios = new ArrayList<Usuario>();
 	}
-	
-    /**
+
+	/**
 	 * Método que obtiene la lista de usuarios.
-	 * @return Lista de los distintos usuarios que se cargaron en la lista con anterioridad.
+	 * @return Lista de los distintos usuarios que se cargaron en la
+	 * lista con anterioridad.
 	 */
 	public ArrayList<Usuario> getListaUsuarios() {
 		return this.listaUsuarios;
 	}
-    /**
-	 * Método que envía la consula SQL de eliminación al agente, si este realiza la operación de manera correcta
-	 * se obtendrá un valor de true como resultado de la ejecución del método.
+
+	/**
+	 * Método que envía la consula SQL de eliminación al agente, si este
+	 * realiza la operación de manera correcta se obtendrá un valor de
+	 * true como resultado de la ejecución del método.
 	 * @param usuario (Usuario que se va a eliminar de la base de datos).
-	 * @return Valor a true o a false en función de si la consulta se ha ejecutado de manera correcta o no.
+	 * @return Valor a true o a false en función de si la consulta se ha
+	 * ejecutado de manera correcta o no.
 	 */
 	public boolean delete(Usuario usuario) {
 		boolean correcto = false;
 		try {
-			Agente.getAgente().modificar("DELETE FROM usuarios WHERE nombre = '"+usuario.getNombre()+"'");
-			correcto=true;
+			Agente.getAgente().modificar("DELETE FROM usuarios "
+					+ "WHERE nombre = '"
+					+ usuario.getNombre() + "'");
+			correcto = true;
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
-			correcto=false;
+			correcto = false;
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
-			correcto=false;
-		}
-		return correcto;
-	}
-    /**
-	 * Método que envía la consula SQL de modificación al agente, si este realiza la operación de manera correcta
-	 * se obtendrá un valor de true como resultado de la ejecución del método.
-	 * @param usuario (Usuario que se va a modificar en la base de datos).
-	 * @return Valor a true o a false en función de si la consulta se ha ejecutado de manera correcta o no.
-	 */
-	public boolean update(Usuario usuario) {
-		boolean correcto = false;
-		try {
-			String SQL = "UPDATE usuarios SET nombre='"+usuario.getNombre()+"', "
-					+ "pass='"+usuario.getPassword()+"',tipo='"+usuario.getTipo()+"'"
-					+ " WHERE nombre = '"+usuario.getNombre()+"' AND pass='"+usuario.getPassword()+"'";
-			Agente.getAgente().modificar(SQL);
-			correcto=true;
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			//e.printStackTrace();
-			correcto=false;
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			correcto=false;
-		}
-		return correcto;
-	}
-    /**
-	 * Método que envía la consula SQL de creación de usuario al agente, si este realiza la operación de manera correcta
-	 * se obtendrá un valor de true como resultado de la ejecución del metodo.
-	 * @param usuario (Usuario que se va a añadir a la base de datos).
-	 * @return Valor a true o a false en función de si la consulta se ha ejecutado de manera correcta o no.
-	 */
-	public boolean insert(Usuario usuario) {
-		boolean correcto=false;
-		try {
-			Agente.getAgente().modificar("INSERT INTO usuarios VALUES ('"+usuario.getNombre()+"',"
-					+ "'"+usuario.getPassword()+"','"+usuario.getTipo()+"')");
-			correcto=true;
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			correcto=false;
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			correcto=false;
+			correcto = false;
 		}
 		return correcto;
 	}
 
-    /**
-	 * Método que envía la consula SQL de lectura al agente para comprobar de la existencia
-	 * un usuario según los datos introducidos, si este realiza la operación de manera correcta
-	 * se obtendrá un valor de true como resultado de la ejecución del método.
-	 * @param usuario (Usuario sobre el cual se quieren comprobar sus datos para realizar el login).
-	 * @return Valor a true o a false en funcion de si la consulta se ha ejecutado de manera correcta o no.
+	/**
+	 * Método que envía la consula SQL de modificación al agente, si este
+	 * realiza la operación de manera correcta se obtendrá un valor
+	 * de true como resultado de la ejecución del método.
+	 * @param usuario (Usuario que se va a modificar en la base de datos).
+	 * @return Valor a true o a false en función de si la consulta se ha
+	 * ejecutado de manera correcta o no.
+	 */
+	public boolean update(Usuario usuario) {
+		boolean correcto = false;
+		try {
+			String SQL = "UPDATE usuarios SET nombre='" 
+					+ usuario.getNombre() + "', " 
+					+ "pass='" + usuario.getPassword() 
+					+ "',tipo='" + usuario.getTipo() + "'" 
+					+ " WHERE nombre = '"
+					+ usuario.getNombre() + "' AND pass='"
+					+ usuario.getPassword() + "'";
+			Agente.getAgente().modificar(SQL);
+			correcto = true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			// e.printStackTrace();
+			correcto = false;
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			correcto = false;
+		}
+		return correcto;
+	}
+
+	/**
+	 * Método que envía la consula SQL de creación de usuario al agente,
+	 * si este realiza la operación de manera correcta se obtendrá un
+	 * valor de true como resultado de la ejecución del metodo.
+	 * @param usuario (Usuario que se va a añadir a la base de datos).
+	 * @return Valor a true o a false en función de si la consulta se ha
+	 * ejecutado de manera correcta o no.
+	 */
+	public boolean insert(Usuario usuario) {
+		boolean correcto = false;
+		try {
+			Agente.getAgente().modificar("INSERT INTO usuarios VALUES ('"
+					+ usuario.getNombre() + "'," + "'" 
+					+ usuario.getPassword()
+					+ "','" + usuario.getTipo() + "')");
+			correcto = true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			correcto = false;
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			correcto = false;
+		}
+		return correcto;
+	}
+
+	/**
+	 * Método que envía la consula SQL de lectura al agente para comprobar
+	 * de la existencia un usuario según los datos introducidos, si este
+	 * realiza la operación de manera correcta se obtendrá un valor de true
+	 * como resultado de la ejecución del método.
+	 * @param usuario (Usuario sobre el cual se quieren comprobar sus datos
+	 * para realizar el login).
+	 * @return Valor a true o a false en funcion de si la consulta se ha
+	 *         ejecutado de manera correcta o no.
 	 */
 	public boolean login(Usuario usuario) {
 		ResultSet resultado;
-		boolean existe=false;
+		boolean existe = false;
 		try {
-			resultado=Agente.getAgente().leer("SELECT * FROM usuarios WHERE nombre='"+usuario.getNombre()+"'"
-					+ " AND pass='"+usuario.getPassword()+"'");
-			
-			while(resultado.next()) {
-				existe=true;
+			resultado = Agente.getAgente()
+					.leer("SELECT * FROM usuarios WHERE nombre='"
+							+ usuario.getNombre() + "'" + " AND pass='"
+							+ usuario.getPassword() + "'");
+
+			while (resultado.next()) {
+				existe = true;
 				usuario.setNombre(resultado.getString(1));
 				usuario.setPassword(resultado.getString(2));
 				usuario.setTipo(resultado.getString(3));
-				
+
 			}
-			
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
-			existe=false;
+			existe = false;
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
-			existe=false;
+			existe = false;
 		}
 		return existe;
 	}
-    /**
-	 * Método que envía la consula SQL de lectura al agente para obtener los datos referentes a todos los 
-	 * usuarios, una vez se tengan los datos se iran creando usuarios y estos se añadiran a la lista.
-	 * @return Valor a true o a false en funcion de si la consulta se ha ejecutado de manera correcta o no.
+
+	/**
+	 * Método que envía la consula SQL de lectura al agente para obtener los
+	 * datos referentes a todos los usuarios, una vez se tengan los datos se
+	 * iran creando usuarios y estos se añadiran a la lista.
+	 * @return Valor a true o a false en funcion de si la consulta se ha
+	 *         ejecutado de manera correcta o no.
 	 */
 	public boolean readAll() {
-	ResultSet resultado;
-	boolean correcto = false;
+		ResultSet resultado;
+		boolean correcto = false;
 		try {
 			resultado = Agente.getAgente().leer("SELECT * FROM usuarios");
 			Usuario usuario;
-			
-			while (resultado.next()){
-				correcto=true;
-				usuario=new Usuario(resultado.getString(1),resultado.getString(2),resultado.getString(3));
+
+			while (resultado.next()) {
+				correcto = true;
+				usuario = new Usuario(resultado.getString(1),
+						resultado.getString(2), resultado.getString(3));
 				listaUsuarios.add(usuario);
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
-			correcto=false;
+			correcto = false;
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
-			correcto=false;
+			correcto = false;
 		}
 		return correcto;
 	}
-	
+
 }
