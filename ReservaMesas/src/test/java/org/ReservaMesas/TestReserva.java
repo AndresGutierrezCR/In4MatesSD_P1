6b5d1f4b6cca062eb6dec4889deccd7a8877931c
DavidCarneros <David.Carneros1@alu.uclm.es>
2018-12-19 10:50:48 +0100

    Tests terminados.

--- ReservaMesas/src/test/java/org/ReservaMesas/TestReserva.java
+++ ReservaMesas/src/test/java/org/ReservaMesas/TestReserva.java
@@ -7,7 +7,6 @@ import org.ReservaMesas.Dominio.Estados;
 import org.ReservaMesas.Dominio.Mesa;
 import org.ReservaMesas.Dominio.Reserva;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class TestReserva {
@@ -15,188 +14,199 @@ public class TestReserva {
 	private Mesa mesaValida;
 	
 	@Before
-	public void setUp() throws Exception {
+	public void setUp() throws Exception{
 		mesaValida = new Mesa(2, 6, Estados.LIBRE, "01/01/2019 12:00:00");
 	}
-
+	
 	@Test(expected = Exception.class)
 	public void test1() throws Exception {
-		Reserva r = new Reserva(-3, "pepe", -5, "comida", -7, mesaValida);
+		Reserva r = new Reserva(-3, "pepe", -5, "comida", -8, mesaValida);
 	}
-
 	@Test(expected = Exception.class)
 	public void test2() throws Exception {
-		Reserva r = new Reserva(0, "pepe", -5, "comida", -7, mesaValida);
+		Reserva r = new Reserva(100, "", 2, "cena", 2, mesaValida);
 	}
-
 	@Test(expected = Exception.class)
 	public void test3() throws Exception {
-		Reserva r = new Reserva(0, "pepe", 0, "comida", -7, mesaValida);
+		Reserva r = new Reserva(0, "pepe", 0, "", 2, null);
 	}
-
 	@Test(expected = Exception.class)
 	public void test4() throws Exception {
-		Reserva r = new Reserva(100, "", 0, "comida", -7, mesaValida);
+		Reserva r = new Reserva(-3, "", 100, "comida", 1, mesaValida);
 	}
-
 	@Test(expected = Exception.class)
 	public void test5() throws Exception {
-		Reserva r = new Reserva(100, "", 0, "cena", -7, mesaValida);
+		Reserva r = new Reserva(100, "pepe", -5, "cena", -8, mesaValida);
 	}
-
 	@Test(expected = Exception.class)
 	public void test6() throws Exception {
-		Reserva r = new Reserva(100, "Pepe", 2, "", -7, mesaValida);
+		Reserva r = new Reserva(-3, "pepe", 0, "comida", -8, mesaValida);
 	}
 
-	@Test(expected = Exception.class)
 	public void test7() throws Exception {
-		Reserva r = new Reserva(100, "Pepe", 2, "comida", 0, mesaValida);
+		try {
+		Reserva r = new Reserva(100, "pepe", 2, "cena",2, mesaValida);
+		}catch(Exception e) {
+			assumeNoException(e);
+		}
 	}
-
 	@Test(expected = Exception.class)
 	public void test8() throws Exception {
-		Reserva r = new Reserva(-3, "Pepe", 2, "cena", 10, mesaValida);
+		Reserva r = new Reserva(0, "", -5, "comida", 1, mesaValida);
 	}
-
 	@Test(expected = Exception.class)
 	public void test9() throws Exception {
-		Reserva r = new Reserva(100, "pepe", 2, "prueba", 10, mesaValida);
+		Reserva r = new Reserva(-3, "pepe", 2, "cena", 1, mesaValida);
 	}
-
 	@Test(expected = Exception.class)
 	public void test10() throws Exception {
-		Reserva r = new Reserva(100, "Pepe", 2, "comida", 10, null);
+		Reserva r = new Reserva(100, "pepe", -5, "comida", 0, mesaValida);
 	}
-
-	@Test
-	public void test11() {
-		try {
-			Reserva r = new Reserva(4, "Manolo", 2, "cena", 2, mesaValida);
-		} catch (Exception e) {
-			
-			assumeNoException(e);
-		}
+	@Test(expected = Exception.class)
+	public void test11() throws Exception {
+		Reserva r = new Reserva(0, "", -5, "", 2, mesaValida);
 	}
+	
 	@Test(expected = Exception.class)
 	public void test12() throws Exception {
-		
-		Reserva r = new Reserva(4, "Manolo", 200, "comida", 2, mesaValida);
-		
+		Reserva r = new Reserva(100, "pepe", -5, "comida", 2, mesaValida);
 	}
-
-	@Test
-	public void TestGetters() throws Exception {
-		Reserva r = new Reserva(4, "Manolo", 2, "comida", 2, mesaValida);
-		try {
-			assertEquals(4, r.getIdReserva());
-			assertEquals("Manolo", r.getNombreCliente());
-			assertEquals(2, r.getComensales());
-			assertEquals("comida", r.getTurnoComCen());
-			assertEquals(2, r.getTurno());
-		} catch (Exception e) {
-			assumeNoException(e);
-		}
-
-	}
-
-	@Test
-	public void TestSetNombre() {
-		try {
-			Reserva r = new Reserva(4, "Manolo", 2, "comida", 2, mesaValida);
-			r.setNombreCliente("Manuel");
-		} catch (Exception e) {
-			assumeNoException(e);
-		}
-	}
-
 	@Test(expected = Exception.class)
-	public void TestSetNombreFalla()throws Exception {
+	public void test13() throws Exception {
+		Reserva r = new Reserva(100, "pepe", 2, "cena", 0, mesaValida);
+	}
+	@Test(expected = Exception.class)
+	public void test14() throws Exception {
+		Reserva r = new Reserva(100, "pepe", 2, "", 2, mesaValida);
+	}
+	@Test
+	public void test15() throws Exception {
+		try {
+		Reserva r = new Reserva(100, "pepe", 2, "comida", 2, mesaValida);
+		}catch(Exception e) {
+			assumeNoException(e);
+		}
+	}
+
+	/* Test set idReserva */
+	@Test(expected = Exception.class)
+	public void testsetIdReserva1() throws Exception {
+		Reserva r = new Reserva();
+		r.setIdReserva(-3);
+	}
+	@Test(expected = Exception.class)
+	public void testsetIdReserva2() throws Exception {
+		Reserva r = new Reserva();
+		r.setIdReserva(0);
+	}
+	@Test
+	public void testsetGetIdReserva3() throws Exception {
+		Reserva r = new Reserva();
+		try {
+		r.setIdReserva(100);
+		}catch(Exception e) {
+			assumeNoException(e);
+		}
+		assertEquals(r.getIdReserva(),100);
+	}
 	
-			Reserva r = new Reserva(4, "Manolo", 2, "comida", 2, mesaValida);
-			r.setNombreCliente("");
-		
-	}
-
-	@Test
-	public void TestSetIDreserva() {
-		try {
-			Reserva r = new Reserva(4, "Manolo", 2, "comida", 2, mesaValida);
-			r.setIdReserva(3);
-		} catch (Exception e) {
-			assumeNoException(e);
-		}
-	}
+	/* Test set NombreCliente*/
 
 	@Test(expected = Exception.class)
-	public void TestSetIDreservaFalla() throws Exception {
-
-		Reserva r = new Reserva(4, "Manolo", 2, "comida", 2, mesaValida);
-		r.setIdReserva(-10);
-
+	public void testsetNombreCliente1() throws Exception {
+		Reserva r = new Reserva();
+		r.setNombreCliente("");
 	}
-
 	@Test
-	public void TestSetComensales() {
+	public void testsetGetNombreCliente2() throws Exception {
+		Reserva r = new Reserva();
 		try {
-			Reserva r = new Reserva(4, "Manolo", 2, "comida", 2, mesaValida);
-			r.setComensales(4);
-		} catch (Exception e) {
+		r.setNombreCliente("pepe");
+		}catch(Exception e) {
 			assumeNoException(e);
 		}
+		assertEquals(r.getNombreCliente(),"pepe");
 	}
-
+	
+	/* Tests set comensales */
 	@Test(expected = Exception.class)
-	public void TestSetComensalesFalla() throws Exception {
-
-		Reserva r = new Reserva(4, "Manolo", 2, "comida", 2, mesaValida);
-		r.setComensales(-4);
-
+	public void testsetComensales1() throws Exception {
+		Reserva r = new Reserva(1, "pepe",3 , "comida", 2, mesaValida);
+		r.setComensales(-5);
 	}
-
+	@Test(expected = Exception.class)
+	public void testsetComensales2() throws Exception {
+		Reserva r = new Reserva(1, "pepe",3 , "comida", 2, mesaValida);
+		r.setComensales(0);
+	}
+	@Test(expected = Exception.class)
+	public void testsetComensales3() throws Exception {
+		Reserva r = new Reserva(1, "pepe",3 , "comida", 2, mesaValida);
+		r.setComensales(100);
+	}
 	@Test
-	public void TestSetTurnoComCem() {
+	public void testSetGetComensales4() throws Exception {
+		Reserva r = new Reserva(1, "pepe",3 , "comida", 2, mesaValida);
 		try {
-			Reserva r = new Reserva(4, "Manolo", 2, "comida", 2, mesaValida);
-			r.setTurnoComCen("cena");
-		} catch (Exception e) {
+		r.setComensales(2);
+		}catch(Exception e) {
 			assumeNoException(e);
 		}
+		assertEquals(2, r.getComensales());
 	}
-
+	
+	/* set turn com cen */
+	
 	@Test(expected = Exception.class)
-	public void TestSetTurnoComCemFalla() throws Exception {
-
-		Reserva r = new Reserva(4, "Manolo", 2, "comida", 2, mesaValida);
+	public void testsetturncomcen1() throws Exception {
+		Reserva r = new Reserva();
 		r.setTurnoComCen("");
-
 	}
-	@Test(expected = Exception.class)
-	public void TestSetTurnoComCemFalla2() throws Exception {
-
-		Reserva r = new Reserva(4, "Manolo", 2, "comida", 2, mesaValida);
-		r.setTurnoComCen("prueba");
-
-	}
-
 	@Test
-	public void TestSetTurno() {
+	public void testsetturncomcen2() throws Exception {
+		Reserva r = new Reserva();
 		try {
-			Reserva r = new Reserva(4, "Manolo", 2, "comida", 2, mesaValida);
-			r.setTurno(5);
-		} catch (Exception e) {
+			r.setTurnoComCen("comida");
+		}catch(Exception e) {
 			assumeNoException(e);
 		}
 	}
-
-	@Test(expected = Exception.class)
-	public void TestSetTurnoFalla() throws Exception {
-
-		Reserva r = new Reserva(4, "Manolo", 2, "comida", 2, mesaValida);
-		r.setTurno(-5);
-
+	
+	@Test
+	public void testGetSetturncomcen3() throws Exception {
+		Reserva r = new Reserva();
+		try {
+			r.setTurnoComCen("cena");
+		}catch(Exception e) {
+			assumeNoException(e);
+		}
+		assertEquals("cena",r.getTurnoComCen());
 	}
 	
+	/* test set turno*/
+	
+	@Test(expected = Exception.class)
+	public void testsetturno1() throws Exception {
+		Reserva r = new Reserva();
+		r.setTurno(-8);
+	}
+	@Test(expected = Exception.class)
+	public void testsetturno2() throws Exception {
+		Reserva r = new Reserva();
+		r.setTurno(0);
+	}
+	@Test
+	public void testsetgetturno3() throws Exception {
+		Reserva r = new Reserva();
+		try {
+		r.setTurno(2);
+		}catch(Exception e) {
+			assumeNoException(e);
+		}
+		assertEquals(2,r.getTurno());
+	}
+	
+	/* Test Operaciones CRUD*/
 	
 	@Test
 	public void TestOperacionesCRUD() {
